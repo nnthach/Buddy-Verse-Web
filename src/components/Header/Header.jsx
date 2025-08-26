39c6d0fa0caa3d8df07af3fa17cd656b03a33843
nnthach <nguyenngocthach2301@gmail.com>
2025-08-26 21:20:49 +0700

    Setup route & sass

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import styles from './Header.module.scss';
+
+function Header() {
+  return <div className={styles.wrap}>Header</div>;
+}
+
+export default Header;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,10 @@
+import Home from '~/pages/Home/Home';
+
+export const publicRouters = [
+  {
+    path: '/',
+    component: <Home />,
+  },
+];
+
+export const privateRouters = [];
